92cafb2b97987aa9ee85b7e69e82ac06e9f7d7a5
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-30 18:47:15 +0200

    @RequestMapping

--- src/main/java/com/example/springBootMasterClass/customer/CustomerController.java
+++ src/main/java/com/example/springBootMasterClass/customer/CustomerController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(path = "api/v1/customer")
 @RestController
 public class CustomerController {
 
@@ -21,7 +23,7 @@ public class CustomerController {
     this.customerService = customerService;
   }
 
-  @GetMapping
+  @GetMapping(value = "all")
   List<Customer> getCustomer() {
     return customerService.getCustomers();
   }
