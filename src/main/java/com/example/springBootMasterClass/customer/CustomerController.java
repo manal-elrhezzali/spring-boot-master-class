f32659d01724e9b9a12deea17fe8f36b8d9f2b21
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-30 17:59:43 +0200

    GET POST PUT DELETE mappings

--- src/main/java/com/example/springBootMasterClass/customer/CustomerController.java
+++ src/main/java/com/example/springBootMasterClass/customer/CustomerController.java
@@ -2,7 +2,12 @@ package com.example.springBootMasterClass.customer;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +24,23 @@ public class CustomerController {
   @GetMapping
   List<Customer> getCustomer() {
     return customerService.getCustomers();
+  }
+
+  @PostMapping
+  void createNewCustomer(@RequestBody Customer customer) {
+    System.out.println("POST REQUEST....");
+    System.out.println(customer);
+  }
+
+  @DeleteMapping(path = "{customerId}")
+  void deleteUser(@PathVariable("customerId") Long id) {
+    System.out.println("Delete REQUEST....");
+    System.out.println(id);
+  }
+
+  @PutMapping
+  void updateCustomer(@RequestBody Customer customer) {
+    System.out.println("UPDATE REQUEST....");
+    System.out.println(customer);
   }
 }
