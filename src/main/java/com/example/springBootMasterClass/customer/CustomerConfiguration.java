5f7254c32d1fadf6c54f977ad7fd94220dc74d51
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-06-03 13:15:30 +0200

    @Value and Environment

--- src/main/java/com/example/springBootMasterClass/customer/CustomerConfiguration.java
+++ src/main/java/com/example/springBootMasterClass/customer/CustomerConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.springBootMasterClass.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
@@ -11,10 +13,22 @@ public class CustomerConfiguration {
   @Value("${app.useFakeCustomerRepo:false}")
   private Boolean useFakeCustomerRepo;
 
+  @Value("${info.company.name}")
+  private String companyName;
+
+  //field injection is not recommended
+  //you should have a constructor and then
+  //inject the field
+  @Autowired
+  private Environment environment;
+
   @Bean
   CommandLineRunner commandLineRunner() {
     return args -> {
       System.out.println("Command Line Runner");
+      System.out.println("********* "+ companyName + " *********");
+      System.out.println(environment.getProperty("info.company.name"));
+
     };
   }
 
