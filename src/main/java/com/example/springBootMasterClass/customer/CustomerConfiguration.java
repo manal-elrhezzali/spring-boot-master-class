66ef821afcdabd755f804f3d738dbf0d76e9f39c
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-06-03 13:38:00 +0200

    @ConfigurationProperties

--- src/main/java/com/example/springBootMasterClass/customer/CustomerConfiguration.java
+++ src/main/java/com/example/springBootMasterClass/customer/CustomerConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.springBootMasterClass.customer;
 
+import com.example.springBootMasterClass.infoapp.InfoApp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -23,11 +24,12 @@ public class CustomerConfiguration {
   private Environment environment;
 
   @Bean
-  CommandLineRunner commandLineRunner() {
+  CommandLineRunner commandLineRunner(InfoApp infoApp) {
     return args -> {
       System.out.println("Command Line Runner");
       System.out.println("********* "+ companyName + " *********");
       System.out.println(environment.getProperty("info.company.name"));
+      System.out.println(infoApp);
 
     };
   }
