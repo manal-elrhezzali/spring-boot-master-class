6c1c6cee09791602c50f6bd2aad9f34458e4c94c
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-06-03 04:57:30 +0200

    Consuming API with OpenFeign

--- src/main/java/com/example/springBootMasterClass/SpringBootMasterClassApplication.java
+++ src/main/java/com/example/springBootMasterClass/SpringBootMasterClassApplication.java
@@ -2,11 +2,14 @@ package com.example.springBootMasterClass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+//to enable feign clients
+@EnableFeignClients
 public class SpringBootMasterClassApplication {
 
 	public static void main(String[] args) {
